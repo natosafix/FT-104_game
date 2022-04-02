bd6408febb187c3de66aae3019da0dce48e2e814
bolthen <92172378+bolthen@users.noreply.github.com>
2022-04-02 12:36:38 +0500

    Test Commit
    Тест работы Git с Unity

---
+++ Game/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    
+    void Start()
+    {
+        rigidbody2D = rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        DirectionMove();
+    }
+
+    void Move()
+    {
+        var w = Input.GetKey(KeyCode.W) ? 1 : 0;
+        var a = Input.GetKey(KeyCode.A) ? -1 : 0;
+        var s = Input.GetKey(KeyCode.S) ? -1 : 0;
+        var d = Input.GetKey(KeyCode.D) ? 1 : 0;
+        
+        var moveVec = new Vector2(a + d, w + s).normalized;
+
+        rigidbody2D.AddForce(moveVec * 20);
+        rigidbody2D.velocity *= 0.9f;
+    }
+    
+    private void DirectionMove()
+    {
+        rigidbody2D.rotation = Mathf.Atan2(rigidbody2D.velocity.y, rigidbody2D.velocity.x) * Mathf.Rad2Deg;
+    }
+}
