be659b5eaec2043b3b17f75ed8b9db1f30c129b9
natosafix <92174064+natosafix@users.noreply.github.com>
2022-05-13 01:08:45 +0500

    AddFireball

--- Game/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Game/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
 
 
 public enum PlayerStates
 {
     Katana = 0,
-    WithWeapon = 1
+    WithWeapon = 1,
+    CastSpell = 2
 };
 
 
@@ -17,23 +19,30 @@ public class PlayerMove : Entity
 {
     public float ShotDelay = 0.2f;
     public float KatanaDelay = 0.8f;
+    public float FireballDelay = 1.0f;
     public float Acceleration = 40f;
     public GameObject Bullet;
+    public GameObject Fireball;
     public GameObject StartBulletPos;
+    public GameObject FireballStartPos;
     public GameObject Weapon;
     public Animator BodyAnim;
     public PlayerStates State = PlayerStates.Katana;
-    private float coolDown;
+    private float shotCoolDown;
+    private float spellCoolDown;
     private Vector2 moveVec;
     private Vector2 mouseVec;
     private bool isGunInInventory = false;
     private Transform bulletStartPosTransform;
+    private Transform fireballStartPosTransform;
+    private bool isSpellCasted = false;
     
     void Start()
     {
         Weapon.GetComponent<Renderer>().enabled = false;
         SetUp();
         bulletStartPosTransform = StartBulletPos.transform;
+        fireballStartPosTransform = FireballStartPos.transform;
         Enemy.EnemiesSetupTarget(this);
         EnemyAttack.InitialisePlayer(this);
     }
@@ -45,24 +54,33 @@ public class PlayerMove : Entity
         UpdateAnim();
         moveVec = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         mouseVec = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //Attack();
     }
 
     void FixedUpdate()
     {
         if (!IsAlive())
             return;
+        Attack();
+        if (isSpellCasted)
+        {
+            rigidbody2D.velocity = Vector2.zero;
+            return;
+        }
         Move();
         PlayerRotate();
-        Attack();
     }
 
     void UpdateAnim()
     {
-        if (Input.GetKey(KeyCode.Alpha1))
+        Debug.Log(isSpellCasted);
+        Debug.Log(spellCoolDown);
+        if (Input.GetKey(KeyCode.Alpha1) || isSpellCasted && spellCoolDown <= 0)
         {
             State = PlayerStates.Katana;
             BodyAnim.SetInteger("PlayerState", (int) State);
             Weapon.GetComponent<Renderer>().enabled = false;
+            isSpellCasted = false;
         }
         if (Input.GetKey(KeyCode.Alpha2) && isGunInInventory)
         {
@@ -70,27 +88,43 @@ public class PlayerMove : Entity
             Weapon.GetComponent<Renderer>().enabled = true;
             BodyAnim.SetInteger("PlayerState", (int) State);
         }
+        if (Input.GetKey(KeyCode.G) && !isSpellCasted && spellCoolDown <= 0)
+        {
+            State = PlayerStates.CastSpell;
+            Weapon.GetComponent<Renderer>().enabled = false;
+            BodyAnim.SetInteger("PlayerState", (int) State);
+        }
     }
 
     void Attack()
     {
-        if (coolDown > 0)
-            coolDown -= Time.fixedDeltaTime;
+        if (shotCoolDown > 0)
+            shotCoolDown -= Time.fixedDeltaTime;
+        if (spellCoolDown > 0)
+            spellCoolDown -= Time.fixedDeltaTime;
 
         switch (State)
         {
             case PlayerStates.Katana:
-                if (!Input.GetMouseButton((int) MouseButton.LeftMouse) || coolDown > 0)
+                if (!Input.GetMouseButton((int) MouseButton.LeftMouse) || shotCoolDown > 0)
                     break;
                 BodyAnim.SetTrigger("KatanaAttack");
-                coolDown = KatanaDelay;
+                shotCoolDown = KatanaDelay;
                 rigidbody2D.AddForce(Vector2.up.Rotate(rigidbody2D.rotation) * 10, ForceMode2D.Impulse);
                 break;
             case PlayerStates.WithWeapon:
-                if (!Input.GetMouseButton((int) MouseButton.LeftMouse) || coolDown > 0)
+                if (!Input.GetMouseButton((int) MouseButton.LeftMouse) || shotCoolDown > 0)
                     break;
                 Instantiate(Bullet, bulletStartPosTransform.position, Quaternion.Euler(0, 0, rigidbody2D.rotation));
-                coolDown = ShotDelay;
+                shotCoolDown = ShotDelay;
+                break;
+            case PlayerStates.CastSpell:
+                if (spellCoolDown > 0 || isSpellCasted)
+                    break;
+                BodyAnim.SetTrigger("SpellAttack");
+                isSpellCasted = true;
+                Instantiate(Fireball, fireballStartPosTransform.position, Quaternion.Euler(0, 0, rigidbody2D.rotation));
+                spellCoolDown = FireballDelay;
                 break;
         }
     }
